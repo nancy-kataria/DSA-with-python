37f34dbc8a180d3cb4c4b6d29bb39d1145321695
nancy-kataria <katarianancy8@gmail.com>
2025-04-28 23:41:48 -0700

    added adjacency list

--- Graphs/directed_graph.py
+++ Graphs/directed_graph.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 # Array of Edges (Directed) [Start, End]
 n = 8
 A = [[0, 1], [1, 2], [0, 3], [3, 4], [3, 6], [3, 7], [4, 2], [4, 5], [5, 2]]
@@ -17,4 +19,13 @@ for u,v in A:
     # uncomment this line if the graph is undirected
     # M[v][u] = 1
 
-print(M)
+# print(M)
+
+# Adjacency List
+
+D = defaultdict(list)
+
+for u,v in A:
+    D[u].append(v)
+
+print(D)
