924a63b36bac5756938ee866fbef2288259283ce
Nancy Kataria <65275547+nancy-kataria@users.noreply.github.com>
2025-07-07 22:49:53 -0700

    move zero to end problem

--- Array/array.py
+++ Array/array.py
@@ -109,3 +109,13 @@ def inPlaceRotation(nums, k):
     reverse(k, n-1)
 
     return nums
+
+def moveZeroesToEnd(nums):
+    i = 0
+
+    for j in range(len(nums)):
+        if nums[j] != 0:
+            nums[i], nums[j] = nums[j], nums[i]
+            i+=1
+
+    return nums
